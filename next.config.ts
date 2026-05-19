b645652af3bc8a6d6af25bbec1b4350ee8902e86
Priscilla Jospin <priscillajospin@karunya.edu.in>
2026-05-19 23:16:33 +0530

    chore: add 85 to image qualities configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     formats:     ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    qualities:   [75, 80],
+    qualities:   [75, 80, 85],
   },
 };
 
